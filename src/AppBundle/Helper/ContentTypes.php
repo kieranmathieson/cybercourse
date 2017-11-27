3516a8c40db211fa9742d2e2554a2ed7e49d35de
Kieran Mathieson <mathieso@oakland.edu>
2017-11-27 14:44:37 -0500

    Define constants for content types.

--- src/AppBundle/Helper/ContentTypes.php
+++ src/AppBundle/Helper/ContentTypes.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Helper for content types.
+ *
  * User: kieran
  * Date: 11/5/2017
  * Time: 7:30 AM
@@ -10,28 +11,28 @@ namespace AppBundle\Helper;
 
 class ContentTypes
 {
+    const ALL = 'all';
     const LESSON = 'lesson';
     const EXERCISE = 'exercise';
     const PATTERN = 'pattern';
     const CORE_IDEA = 'coreidea';
     const SITE_PAGE = 'sitepage';
-//    const LEARNING_GROUP = 'lrngroup';
 
     const CONTENT_TYPES = [
+        ContentTypes::ALL => 'all',
         ContentTypes::LESSON => 'lesson',
         ContentTypes::EXERCISE => 'exercise',
         ContentTypes::PATTERN => 'pattern',
         ContentTypes::CORE_IDEA => 'coreidea',
         ContentTypes::SITE_PAGE => 'sitepage',
-//        ContentTypes::LEARNING_GROUP => 'Learning group',
     ];
 
     const CONTENT_TYPE_DISPLAY_NAMES = [
+        ContentTypes::ALL => 'All',
         ContentTypes::LESSON => 'Lesson',
         ContentTypes::EXERCISE => 'Exercise',
         ContentTypes::PATTERN => 'Pattern',
         ContentTypes::CORE_IDEA => 'Core idea',
         ContentTypes::SITE_PAGE => 'Site page',
-//        ContentTypes::LEARNING_GROUP => 'Learning group',
     ];
 }
