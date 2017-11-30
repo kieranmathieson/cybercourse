3882e918105df6f78ea52cce0c14089ec450b0a1
Kieran Mathieson <mathieso@oakland.edu>
2017-11-30 15:47:56 -0500

    Helper for working with DataTables on the client.

---
+++ src/AppBundle/Helper/DataTableHelper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kieran
+ * Date: 11/30/2017
+ * Time: 3:32 PM
+ */
+
+namespace AppBundle\Helper;
+
+
+class DataTableHelper
+{
+    /** Standard attributes for showing DataTables. */
+    const STANDARD_ATTRS = 'class="table display stripe-table highlight-sorting-column show-hover-row" width="100%"';
+}
