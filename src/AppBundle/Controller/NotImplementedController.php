42930973be80494a556955509ce9c654a68f66c2
Kieran Mathieson <mathieso@oakland.edu>
2017-11-30 15:49:43 -0500

    Controller to show a not-implemented message.

--- src/AppBundle/Controller/NotImplementedController.php
+++ src/AppBundle/Controller/NotImplementedController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HelperController extends Controller
+class NotImplementedController extends Controller
 {
     /**
      * Tell the user something is not implemented.
