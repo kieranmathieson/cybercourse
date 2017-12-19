e4ba2fa1f3dacf7a60350e0a5ebab586597e01bd
Kieran Mathieson <mathieso@oakland.edu>
2017-12-19 11:52:35 -0500

    Exercise directive for the ReST transformer.

--- src/AppBundle/Service/RstTransformer.php
+++ src/AppBundle/Service/RstTransformer.php
@@ -9,23 +9,41 @@
 namespace AppBundle\Service;
 
 //use Gregwar\RST\Directives\DogBlock;
+use AppBundle\Helper\UserHelper;
 use AppBundle\Service\RstCustomDirectives\Directives\Dude;
+use AppBundle\Service\RstCustomDirectives\Directives\ExerciseDirective;
+use Doctrine\ORM\EntityManager;
 use Gregwar\RST\Parser;
 
 class RstTransformer
 {
-    private $parser;
-    private $dudeDirective;
+    protected $parser;
+    protected $dudeDirective;
+    protected $exerciseDirective;
 
-    public function __construct()
+    /** @var EntityManager $entityManager */
+    protected $entityManager;
+
+    /** @var UserHelper $userHelper */
+    protected $userHelper;
+
+    public function __construct(
+        EntityManager $entityManager,
+        UserHelper $userHelper)
     {
+        $this->entityManager = $entityManager;
+        $this->userHelper = $userHelper;
         $this->dudeDirective = new Dude();
+        $this->exerciseDirective = new ExerciseDirective(
+            $this->entityManager, $this->userHelper, $this);
         $this->parser = new Parser();
         $this->parser->registerDirective($this->dudeDirective);
+        $this->parser->registerDirective($this->exerciseDirective);
     }
 
     public function transform($rstContent) {
-        $transformedHtml = $this->parser->parse($rstContent);
+        $transformedNodes = $this->parser->parse($rstContent);
+        $transformedHtml = $transformedNodes->render();
         return $transformedHtml;
     }
 
